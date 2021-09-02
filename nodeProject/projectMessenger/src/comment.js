f5c21fc27fcf0324b3fb5b89830551b10209001e
dina-03 <dingrafikweb@gmail.com>
2021-09-02 20:11:33 +0200

    corrected code

---
+++ nodeProject/projectMessenger/src/comment.js
@@ -0,0 +1,20 @@
+import { findUserById } from "./data";
+export default class Comment{
+    constructor(userId, text, data){
+        this.id=Comment.id++;
+        this.userId=userId;
+        this.text=text;
+        this.data=data;    
+    }
+    static id=0;
+
+    renderComment(){
+        return `
+        <div>~~~~~~~~~~~~~~~~~~~~~~~~~~~~   
+        <h5>${this.text}</h5> 
+        <p>${this.data},
+        <br> comment by ${findUserById(this.userId).name}</p>
+        </div>
+        `
+    }
+}
---
+++ nodeProject/projectMessenger/src/forms.js
@@ -0,0 +1,50 @@
+export function renderAddMessageForm(userId){
+    return`
+    <h4><b>Write new message</b></h4>
+    <form name = 'message' action = '#'>
+    <input 
+        id ='title' 
+        class = 'message-title'
+        type = 'text'
+        name = 'title'
+        placeholder = 'Title'
+        /><br>
+        <textarea 
+        id ='text' 
+        class = 'textArea'
+        type = 'text'
+        name = 'text'
+        cols="40" rows="2"
+        placeholder = 'Text message'></textarea><br>
+    <button class = 'add-btn' name = 'button' data-user = ${userId}>add message</button>
+    </form>
+    `
+}
+
+export function renderAddCommentForm(messageId){
+    return `    
+    <h4>To write new comment enter you name</h4>
+    <form name = "userName" action = "#">
+            <input
+            id="userName"
+            class = 'message-title'
+            type="text"
+            name="uname"
+            placeholder="Type name"
+        />
+        <button class="add-btn" name = "button">Send</button>
+    </form>
+    <form name = "comment" action = "#" class = "hide">
+            <textarea
+            id="message_${messageId}"
+            class = 'textArea'
+            type="text"
+            name="ucomment"
+            cols="40" rows="2"
+            placeholder="Type comment"
+        ></textarea><br>
+        <button class="add-btn" name ="button">Add comment</button>
+    </form>
+    <h4 id="not-found-user" class ="hide">no such user!</h4>
+`
+}
